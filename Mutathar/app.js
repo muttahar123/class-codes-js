a19069ef90e7ce9539efa6c376133dc492b60962
Muttahar sheikh <153789569+muttahar123@users.noreply.github.com>
2024-03-01 11:13:26 +0500

    done

---
+++ Mutathar/app.js
@@ -0,0 +1,50 @@
+// var num = 8
+// var number = num++
+// console.log(num )
+// console.log(number)
+
+
+// var num = 8
+// var number = ++num
+// console.log(num )
+// console.log(number)
+
+
+// var num = 8
+// var number = num--
+// console.log(num )
+// console.log(number)
+
+
+// var num = 8
+// var number = --num
+// console.log(num )
+// console.log(number)
+
+// var alll = 'habdjka' + 90 + true 
+// console.log(alll)
+
+
+var userinput = prompt("Enter your table number")
+console.log(userinput)
+document.write(`<h4>${userinput}x 1 = ${userinput}</h4>`)
+document.write(`<h4>${userinput}x 2 = ${userinput*2}</h4>`)
+document.write(`<h4>${userinput}x 3 = ${userinput*3}</h4>`)
+document.write(`<h4>${userinput}x 4 = ${userinput*4}</h4>`)
+document.write(`<h4>${userinput}x 5 = ${userinput*5}</h4>`)
+document.write(`<h4>${userinput}x 6 = ${userinput*6}</h4>`)
+document.write(`<h4>${userinput}x 7 = ${userinput*7}</h4>`)
+document.write(`<h4>${userinput}x 8 = ${userinput*8}</h4>`)
+document.write(`<h4>${userinput}x 9 = ${userinput*9}</h4>`)
+document.write(`<h4>${userinput}x 10 = ${userinput*10}</h4>`)
+
+var English= 90
+var urdu= 88
+var maths =30
+Document.write(`<h2>Obtain Marks</h2>
+<p>English= ${English}/marks</p>
+<p>Urdu= ${urdu}/marks</p>
+<p>Maths= ${maths}/marks</p>
+
+
+`)
