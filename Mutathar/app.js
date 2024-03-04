e6a3b5cabe1ccdaadf7e5a16372941c943eb17db
Waleed Shoaib <>
2024-03-04 11:01:26 +0500

    oki

--- Mutathar/app.js
+++ Mutathar/app.js
@@ -25,26 +25,40 @@
 // console.log(alll)
 
 
-var userinput = prompt("Enter your table number")
-console.log(userinput)
-document.write(`<h4>${userinput}x 1 = ${userinput}</h4>`)
-document.write(`<h4>${userinput}x 2 = ${userinput*2}</h4>`)
-document.write(`<h4>${userinput}x 3 = ${userinput*3}</h4>`)
-document.write(`<h4>${userinput}x 4 = ${userinput*4}</h4>`)
-document.write(`<h4>${userinput}x 5 = ${userinput*5}</h4>`)
-document.write(`<h4>${userinput}x 6 = ${userinput*6}</h4>`)
-document.write(`<h4>${userinput}x 7 = ${userinput*7}</h4>`)
-document.write(`<h4>${userinput}x 8 = ${userinput*8}</h4>`)
-document.write(`<h4>${userinput}x 9 = ${userinput*9}</h4>`)
-document.write(`<h4>${userinput}x 10 = ${userinput*10}</h4>`)
+// var userinput = prompt("Enter your table number")
+// console.log(userinput)
+// document.write('Learing table')
+// document.write(`<h4>${userinput}x 1 = ${userinput}</h4>`)
+// document.write(`<h4>${userinput}x 2 = ${userinput*2}</h4>`)
+// document.write(`<h4>${userinput}x 3 = ${userinput*3}</h4>`)
+// document.write(`<h4>${userinput}x 4 = ${userinput*4}</h4>`)
+// document.write(`<h4>${userinput}x 5 = ${userinput*5}</h4>`)
+// document.write(`<h4>${userinput}x 6 = ${userinput*6}</h4>`)
+// document.write(`<h4>${userinput}x 7 = ${userinput*7}</h4>`)
+// document.write(`<h4>${userinput}x 8 = ${userinput*8}</h4>`)
+// document.write(`<h4>${userinput}x 9 = ${userinput*9}</h4>`)
+// document.write(`<h4>${userinput}x 10 = ${userinput*10}</h4>`)
 
-var English= 90
-var urdu= 88
-var maths =30
-Document.write(`<h2>Obtain Marks</h2>
-<p>English= ${English}/marks</p>
-<p>Urdu= ${urdu}/marks</p>
-<p>Maths= ${maths}/marks</p>
+// var English= 90
+// var urdu= 88
+// var maths =30
+// Document.write(`<h2>Obtain Marks</h2>
+// <p>English= ${English}/marks</p>
+// <p>Urdu= ${urdu}/marks</p>
+// <p>Maths= ${maths}/marks</p>
+// <p> <b>Total marks ${maths + urdu + english} </b> / marks</p>
 
 
-`)
+
+var html = +prompt('Enter your Obtained html marks');
+var css = +prompt('Enter your obained css marks');
+var javascript = +prompt('enter your obtained js marks');
+var obtained = html + css + javascript;
+var total = 300;
+var percentage = obtained / total * 100;
+if (percentage >= 80) {
+    console.log('You got A+')
+}else if 
+
+
+console.log(`Your percentage ${percentage}%`);
