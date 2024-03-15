8e55784ed2c62d36fae217c03dbb1288ef04695a
Muttahar sheikh <153789569+muttahar123@users.noreply.github.com>
2024-03-15 08:25:25 +0500

    done

---
+++ muttahar/javascript 15-/script.js
@@ -0,0 +1,12 @@
+var students = ['Ali','Muttahar','saad','ahmed',123]
+// students.pop()
+// console.log(students);
+// students.push("Bhai sahab")
+// students.shift()
+// students.unshift('')
+// students.splice(0,1,'Muttahar')
+var returnValue =students.slice(0)
+console.log(students);
+console.log(returnValue);
+
+var percentages = []
---
+++ muttahar/learning javascript/app.js
@@ -0,0 +1,9 @@
+
+
+// alert('Attenttion you are 18+..?')
+
+
+// alert('ap kahan tay jhony baba???')
+
+alert('welcom to js land \n Happy coding')
+alert('welcome to js land')
