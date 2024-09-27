1c13783405a35bcd7d1cd104a16fefaf95213cca
muttahar123 <itxmuttahar@gmail.com>
2024-09-27 11:07:55 +0500

    added second commit

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Header from "@/Components/Header";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8  gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+     <h1 className="text-center text-green-400">Hello Muttahar!</h1>
+    </div>
+  );
+}
